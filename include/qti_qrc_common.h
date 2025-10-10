c6d8ec5c00d3459e36a72062b98cf52687a904b2
Canfeng Zhuang <czhuang@qti.qualcomm.com>
2025-10-10 15:37:32 +0800

    chore: bump version to 1.1.1
    Signed-off-by: Canfeng Zhuang <czhuang@qti.qualcomm.com>

--- include/qti_qrc_common.h
+++ include/qti_qrc_common.h
@@ -7,26 +7,24 @@
 #ifndef __QTI_QRC_COMMON_H
 #define __QTI_QRC_COMMON_H
 
-#include <stdio.h>
-#include <stdint.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
-#include <string.h>
 
-struct qrc_device_ops {
-	int (*open)(const char *qrc_dev);
-	void (*close)(int);
-	ssize_t (*read)(int, char *, size_t);
-	ssize_t (*write)(int, const char *, size_t);
-	int (*fionread)(int, int *arg);
-	int (*tcflsh)(int);
+struct qrc_device_ops
+{
+  int (*open)(const char * qrc_dev);
+  void (*close)(int);
+  ssize_t (*read)(int, char *, size_t);
+  ssize_t (*write)(int, const char *, size_t);
+  int (*fionread)(int, int * arg);
+  int (*tcflsh)(int);
 };
 
 extern struct qrc_device_ops qrc_uart_ops;
 
 #endif
-
-
-
